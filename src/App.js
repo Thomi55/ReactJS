fb029ae447b50dc2616c318c805ef2f332ea2a53
Thomi55 <thomyeze57@gmail.com>
2022-03-28 09:34:11 -0300

    nuevo js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+    <a class="navbar-brand">Sneakers</a>
+    <a>Categoria 1</a>
+    <a>Categoria 2</a>
+    <a>Categoria 3</a>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
     </div>
   );
 }
